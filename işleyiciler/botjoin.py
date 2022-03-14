3c2d03e04715d4f0d2da59aa64497133159faca9
Aleyivdiiiiiii <100187817+Aleyivdiiiiiii@users.noreply.github.com>
2022-03-14 06:02:25 +0400

    Update and rename handlers/botjoin.py to işleyiciler/botjoin.py

--- işleyiciler/botjoin.py
+++ işleyiciler/botjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "Chen Asistant"
+        user.first_name =  "Galaxy Asistant"
 
     try:
         await USER.join_chat(invitelink)
